d97e901feedd074fdeb3e8368bf4c53089031bf1
Marc Hollander <marchollander@gmail.com>
2021-01-02 16:32:11 +0100

    Add a Laravel Envoy deploy script

---
+++ Envoy.blade.php
@@ -0,0 +1,30 @@
+@setup
+    $productionPath = '/home/marucn1q/subdomains/tado-ciq-bridge';
+    $productionServer = 'mghollander.nl';
+@endsetup
+
+@servers(['local' => '127.0.0.1', 'production' => $productionServer])
+
+@story('deploy')
+    sync-files
+    deploy-actions
+@endstory
+
+@task('sync-files', ['on' => 'local'])
+    rsync . {{ $productionServer }}:{{ $productionPath }} -avlz --delete-after --exclude-from='.rsync-exclude'
+@endtask
+
+@task('deploy-actions', ['on' => 'production'])
+    cd {{ $production_path }}
+
+    php artisan down --render="errors::maintenance"
+
+    {{-- Laravel deployment optimizations (https://laravel.com/docs/8.x/deployment#optimization) --}}
+    php artisan config:cache
+    php artisan route:cache
+    php artisan view:cache
+
+    php artisan migrate --force
+
+    php artisan up
+@endtask
