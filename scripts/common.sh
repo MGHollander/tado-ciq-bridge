7bf3f8a0f7e9d0e4a315ad60780d4d88890a95d4
Marc Hollander <marchollander@gmail.com>
2021-01-05 20:04:25 +0100

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,26 +1,9 @@
 #!/usr/bin/env bash
 
-# Print a text with the date and time prepended in blue.
-#
-# Available colors:
-#
-# default       \e[39m  Use this to return to normal.
-# red           \e[31m
-# green         \e[32m
-# yellow        \e[33m
-# blue          \e[34m
-# magenta       \e[35m
-# cyan          \e[36m
-# light_gray    \e[37m
-# dark_gray     \e[90m
-# light_red     \e[91m
-# light_green   \e[92m
-# light_yellow  \e[93m
-# light_blue    \e[94m
-# light_magenta \e[95m
-# light_cyan    \e[96m
-# white         \e[97m
+# Bash colors and formatting
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
 
+# Print a text with the date and time prepended in blue.
 function log() {
   echo -e "\033[34m[$(date +'%F %T')]\033[0m $1\033[0m"
 }
