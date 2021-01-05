da317b5462f30837d8966898a5a9b07a5be0111c
Marc Hollander <marchollander@gmail.com>
2021-01-05 10:29:33 +0100

    Update link to GitHub project on homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     </div>
 
     <div class="links">
-        <a href="https://github.com/MGHollander/strava-ciq-bridge">GitHub</a>
+        <a href="https://github.com/MGHollander/tado-ciq-bridge">GitHub</a>
         <a href="https://www.tado.com/">tado° website</a>
         <a href="https://my.tado.com/">tado° web app</a>
         <a href="https://developer.garmin.com/connect-iq/overview/">Connect IQ developer docs</a>
