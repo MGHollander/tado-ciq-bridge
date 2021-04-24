337d50c13a2eb1bc6caad380fb3b5846a865c1c9
Marc Hollander <marchollander@gmail.com>
2021-04-24 21:55:16 +0200

    Improve a11y

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,14 @@
 @extends ('layout')
 
 @section ('content')
-    <div class="title">
+    <h1 class="title">
         {{ config('app.name') }}
-    </div>
+    </h1>
 
-    <div class="links">
+    <aside class="links">
         <a href="https://github.com/MGHollander/tado-ciq-bridge">GitHub</a>
         <a href="https://www.tado.com/">tado° website</a>
         <a href="https://my.tado.com/">tado° web app</a>
         <a href="https://developer.garmin.com/connect-iq/overview/">Connect IQ developer docs</a>
-    </div>
+    </aside>
 @endsection
